c329869b1753b064ecf23fc50852d2836cede4ec
Henry Song <henry.song@samsung.com>
2013-08-20 08:31:19 -0700

    gl: remove glDrawBuffer and glReadBuffer for glesv3
    There is no need to all these call once glBindFramebuffer is called

--- src/cairo-gl-device.c
+++ src/cairo-gl-device.c
@@ -503,7 +503,6 @@ _cairo_gl_ensure_framebuffer (cairo_gl_context_t *ctx,
                               cairo_gl_surface_t *surface)
 {
     GLenum status;
-    GLenum buf;
     cairo_gl_dispatch_t *dispatch = &ctx->dispatch;
 
     if (likely (surface->fb))
@@ -532,10 +531,6 @@ _cairo_gl_ensure_framebuffer (cairo_gl_context_t *ctx,
 
 #if CAIRO_HAS_GL_SURFACE
     glDrawBuffer (GL_COLOR_ATTACHMENT0);
-    glReadBuffer (GL_COLOR_ATTACHMENT0);
-#elif CAIRO_HAS_GLESV3_SURFACE
-    buf = GL_COLOR_ATTACHMENT0;
-    glDrawBuffers (1, &buf);
     glReadBuffer (GL_COLOR_ATTACHMENT0);
 #endif
 
@@ -906,9 +901,6 @@ _cairo_gl_context_set_destination (cairo_gl_context_t *ctx,
                                    cairo_bool_t multisampling)
 {
     cairo_bool_t changing_surface, changing_sampling;
-#if CAIRO_HAS_GLESV3_SURFACE
-    GLenum buf;
-#endif
 
     /* The decision whether or not to use multisampling happens when
      * we create an OpenGL ES surface, so we can never switch modes. */
@@ -945,10 +937,6 @@ _cairo_gl_context_set_destination (cairo_gl_context_t *ctx,
 #if CAIRO_HAS_GL_SURFACE
 	glDrawBuffer (GL_BACK_LEFT);
 	glReadBuffer (GL_BACK_LEFT);
-#elif CAIRO_HAS_GLESV3_SURFACE
-	buf = GL_BACK;
-	glDrawBuffers (1, &buf);
-	glReadBuffer (GL_BACK);
 #endif
     }
 
